a11e72408cbf05181a105b89bf097447de52f481
santangelx <alexandre.santangelo@epfl.ch>
2022-10-27 03:27:51 +0200

    Changed tx

--- implementations.py
+++ implementations.py
@@ -315,7 +315,7 @@ def reg_logistic_regression(y, x, lambda_, inital_w, max_iters, gamma):
     losses = []
 
     w = inital_w
-    tx = np.c_[np.ones((y.shape[0], 1)), x]
+    tx = np.c_[np.ones((w.shape[0], 1)), x]
     
 
     # start the logistic regression
