d850198419ac0fa1c3f42e138b2d1441cc6bc66d
santangelx <alexandre.santangelo@epfl.ch>
2022-10-27 03:00:37 +0200

    dummy test

--- implementations.py
+++ implementations.py
@@ -246,7 +246,7 @@ def penalized_logistic_regression(y, tx, w, lambda_):
 
     """
 
-    loss = calculate_loss(y, tx, w) 
+    loss = calculate_loss(y, tx, w) + (w.T@w) * lambda_ 
     gradient = compute_gradient(y, tx, w) + 2 * lambda_ * w
     return loss,gradient
 
